fecd658caf736bcd112c1bf36b430dece6e5aba1
ithandmai <135786257+ithandmai@users.noreply.github.com>
2023-07-20 21:48:55 +0700

    Update Homework.cpp

--- Homework.cpp
+++ Homework.cpp
@@ -1,1 +1,32 @@
-Hello
+#include <iostream>
+
+bool isLeapYear(int year) {
+    
+    if (year % 4 == 0) {
+        
+        if (year % 100 != 0 || year % 400 == 0) {
+            return true; 
+        }
+    }
+    return false; 
+}
+
+int main() {
+    int thYear, adYear;
+
+    // input
+    std::cout << "Input TH year: ";
+    std::cin >> thYear;
+
+    // change year
+    adYear = thYear - 543;
+
+    //  check_leap year 
+    if (isLeapYear(adYear)) {
+        std::cout << adYear << " is a leap year" << std::endl;
+    } else {
+        std::cout << adYear << " is not a leap year" << std::endl;
+    }
+
+    return 0;
+}
